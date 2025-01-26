b47d7abf301d4e9a5d108211c7f966e745debb21
Mohamed Sameer <md.sameer.dev@gmail.com>
2025-01-26 13:13:07 +0530

    Push all files to GitHub

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/letsbuildplatfrom/', // Ensure this matches your GitHub repository name
-})
+  base: '/letsbuild/', 
+  build:{
+    outDir:'build',}
+  }
+)
